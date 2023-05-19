aa6a384665c00d96eb97ec1a0760f22f4c314c7e
yosikez <yosikez@gmail.com>
2023-05-19 16:17:09 +0700

    fix activity and location

--- src/com/red/processes/AssignToProcess.java
+++ src/com/red/processes/AssignToProcess.java
@@ -17,7 +17,8 @@ public class AssignToProcess extends SvrProcess {
 	
 	private int A_Asset_ID;
 	private int AD_User_ID;
-	
+	private int C_Activity_ID;
+	private int C_Location_ID;
 
 	@Override
 	protected void prepare() {
@@ -30,6 +31,10 @@ public class AssignToProcess extends SvrProcess {
 				AD_User_ID = param.getParameterAsInt();
 			} else if(paramName.equalsIgnoreCase("A_Asset_ID")) {
 				A_Asset_ID = param.getParameterAsInt();
+			} else if(paramName.equalsIgnoreCase("C_Activity_ID")) {
+				C_Activity_ID = param.getParameterAsInt();
+			} else if(paramName.equalsIgnoreCase("C_Location_ID")) {
+				C_Location_ID = param.getParameterAsInt();
 			} else {
 				log.log(Level.SEVERE, "Unknown Parameter: " +  paramName);				
 			}
@@ -52,12 +57,13 @@ public class AssignToProcess extends SvrProcess {
 			asset.saveEx();
 			
 			assetAssignment.setA_Asset_ID(A_Asset_ID);
+			assetAssignment.setC_Activity_ID(C_Activity_ID);
+			assetAssignment.setC_Location_ID(C_Location_ID);
 			assetAssignment.setAssigment_Date(new Timestamp(System.currentTimeMillis()));
 			assetAssignment.setIsAssigned(true);
 			assetAssignment.setAD_User_ID(AD_User_ID);
 			assetAssignment.setName(user.getName());
 			assetAssignment.setAD_Org_ID(user.getAD_Org_ID());
-			assetAssignment.setC_Location_ID(user.getC_Location_ID());
 			assetAssignment.saveEx();
 
 			addLog(getProcessInfo().getAD_Process_ID(), new Timestamp(System.currentTimeMillis()), new BigDecimal(asset.getA_Asset_ID()), "Asset Name : " + asset.getName());
